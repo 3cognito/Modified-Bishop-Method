009c364828d2c0ac51a12bd90253bacaa2221eff
Tommy <tommyubongabasijohn@gmail.com>
2023-10-09 12:23:35 +0100

    further logs

--- src/utilities/average.ts
+++ src/utilities/average.ts
@@ -3,6 +3,7 @@ export function calculateAverage(numbers: { top: number; bottom: number }[]): {
   bottomSum: number;
   length: number;
 } {
+  console.log(numbers);
   const topSum = numbers.reduce((acc, currentValue) => acc + currentValue.top, 0);
   const bottomSum = numbers.reduce((acc, currentValue) => acc + currentValue.bottom, 0);
   const length = numbers.length;
